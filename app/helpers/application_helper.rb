b64bb696ee4ed28b8b83a695d65f438f2cdadaf1
Daniel Fone <daniel@fone.net.nz>
2014-10-27 13:22:57 +1300

    Tidier display

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def first_line(string)
+    lines = String(string).split("\n")
+    initial = truncate lines.first, length: 100
+    more = lines.count > 1 ? content_tag(:span, "+ #{lines.count-1} more lines", class: 'text-muted') : ""
+    safe_join [initial, more.html_safe]
+  end
+
 end
