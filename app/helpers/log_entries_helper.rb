63c7dbc46a795c49b7a9957986068fe56700a564
Daniel Fone <daniel@fone.net.nz>
2015-02-21 14:55:37 +1300

    Add edit link to exceptions

--- app/helpers/log_entries_helper.rb
+++ app/helpers/log_entries_helper.rb
@@ -2,4 +2,11 @@ module LogEntriesHelper
   def scoped_submenu_item(name, scope)
     submenu_item name, { scope: scope }, entries_scope == scope
   end
+
+  def maybe_edit_link(entry)
+    return unless admin?
+    content_tag :div, class: 'pull-right' do
+      link_to 'Edit', polymorphic_url([:admin, entry], action: :edit)
+    end
+  end
 end
