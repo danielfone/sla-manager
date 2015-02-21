0bd6db825bd98cc9b67687f4c95fc197267087c3
Daniel Fone <daniel@fone.net.nz>
2015-02-21 13:51:10 +1300

    Restrict report entries by date

--- app/controllers/admin/reports_controller.rb
+++ app/controllers/admin/reports_controller.rb
@@ -4,8 +4,9 @@ module Admin
     def new
       @report = Report.new do |r|
         r.repository = repository
-        r.report_date = (Date.today - 1.month).end_of_month
-        r.log_entries = repository.log_entries.unpublished
+        r.report_date = params[:date] || default_date
+        r.report_date = r.report_date.end_of_month
+        r.log_entries = default_log_entries.where('completed_at <= ?', r.report_date)
       end
     end
 
@@ -13,6 +14,14 @@ module Admin
 
     def repository
       @repository ||= Repository.find params[:repository_id] if params[:repository_id]
+    end
+
+    def default_date
+      Date.today - 1.month
+    end
+
+    def default_log_entries
+      repository.log_entries.unpublished
     end
 
   end
